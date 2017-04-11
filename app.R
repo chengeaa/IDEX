ec7aac42aee05f2a25b5d79fb282dc23b8b4ca55
Erik Cheng <ckaiwen2@hotmail.com>
2017-04-10 20:10:37 -0700

    column subsetting

--- app.R
+++ app.R
@@ -16,6 +16,7 @@ camps = read_csv("Total Campaigns.csv")
 merge1 = merge(donations, camps, by.x = "Campaign", by.y = "Campaign Name")
 merge2 = merge(merge1, orghistory, by.x = "Organization Name", by.y = "Organization Name")
 
+contactinfo = names(orghistory)
 aggfuncs = vector(mode = "list", length = 4)
 names(aggfuncs) = c("Mean", "Max", "Min", "Sum")
 aggfuncs[[1]] = mean
@@ -25,11 +26,17 @@ aggfuncs[[4]] = sum
 
 ui <- fluidPage(
   
-   titlePanel("IDEX data subsetting"),
-   
+   titlePanel("IDEX data subsetting"), 
    sidebarLayout(
       sidebarPanel(
-         selectInput(
+        selectInput(
+          inputId = "columns",
+          label = "Select:",
+          choices = c("all", names(merge2)),
+          selected = "all",
+          multiple = TRUE
+        ),
+        selectInput(
            inputId = "groupLabel",
            label = "Group by:",
            choices = c("all", names(merge2)),
@@ -50,6 +57,18 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
+  df= merge2
+  observeEvent(input$columns, {
+    if(input$columns == "all"){
+      df = merge2
+    } else {
+      present = names(merge2) %in% input$columns
+      cols = which(present)
+      
+      df = merge2[,cols]
+    }
+    output$mytable = renderDataTable(df)
+  })
   observeEvent(c(input$groupLabel, input$agg),{
     if(input$groupLabel != "all"){
       index = match(input$groupLabel, names(merge2))
@@ -73,11 +92,11 @@ server <- function(input, output) {
       
       output$mytable = renderDataTable(df)
     } else {
-      output$mytable = renderDataTable(merge2)
+      output$mytable = renderDataTable(df)
       
     }
     
-    
+  output$mytable = renderDataTable(df)
   })
 
 }
